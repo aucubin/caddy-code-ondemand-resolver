144631183c9c4b9fb23f35be95b87d98e47a0cee
Christopher Wötzel <aucubin@aucubin.de>
2021-10-30 11:05:28 +0200

    change readme and add ondemand-resolver for code-server and caddy

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Caddy Code Ondemand Resolver
 
-Declares a basic http api that is used for proxing subdomains for code server
+## Idea
+When using code-server you can access ports on the machine via the <port>.<machine-name> endpoint.
+As Caddy does not allow wildcard certificates to be created with the normal http challenge for an acme endpoint on-demand tls needs to be used in this scenario.
+To make on-demand tls more secure a http api can be used that caddy then asks before requesting a certificate for a subdomain.
+This Resolver just contains a basic http api that returns 200 OK if the subdomain is a port number and 403 Forbidden if it not.
+
+Note: The API does not contain any authentication and should only be exposed directly internally on the machine where the caddy server is running.
---
+++ ondemand-resolver.py
@@ -0,0 +1,57 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import getopt
+import os
+import sys
+import time
+import threading
+
+class ResolverServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if(self.path.startswith("/domain")):
+            pathArgs = self.path.split('?')
+            if(len(pathArgs) == 2):
+                queryArgs = pathArgs[1].split('=')
+                if(len(queryArgs) == 2 and queryArgs[0] == 'domain'):
+                    if(queryArgs[1].isdigit()):
+                        self.send_response(200)
+                        self.end_headers()
+                        return
+                    else:
+                        self.send_response(404)
+                        self.end_headers()
+                        return
+            self.send_response(400)
+            self.end_headers()
+        else:
+            self.send_response(500)
+            self.end_headers()
+            self.wfile.write(bytes("Undefined Endpoint", "utf-8"))
+
+if __name__ == "__main__":
+    hostName = ''
+    serverPort = -1
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "s:p:", ["server=","port="])
+
+        for opt, arg in opts:
+            if opt in ['-s']:
+                hostName = arg
+            elif opt in ['-p']:
+                serverPort = int(arg)
+
+    except getopt.GetoptError:
+        print(getopt.GetoptError)
+        print('ondemand-resolver.py -p <port> -s <server>')
+        sys.exit(2)
+
+    webServer = HTTPServer((hostName, serverPort), ResolverServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
